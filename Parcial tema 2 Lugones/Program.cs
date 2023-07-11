1dcefde626e3c057393ee98b618f2788f534c0ae
Usuario <Usuario@GABINETE-08>
2023-07-11 18:55:23 -0300

    Agregar archivos de proyecto.

---
+++ Parcial tema 2 Lugones/Program.cs
@@ -0,0 +1,27 @@
+﻿double area, volumen, altura, Base, lado, arista, contador=0;
+
+do
+{
+    Console.WriteLine("ingrese el valor de la altura de la piramide cuadrada");
+    altura = double.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese el valor de la base de la piramide cuadrada");
+    Base = double.Parse(Console.ReadLine());
+    if(altura ==0 && Base == 0)
+    {
+        break;
+
+    }
+
+    contador++;
+
+    lado = Base / 4;
+    area = lado * (lado + Math.Sqrt(4 * Math.Pow(altura, 2) + Math.Pow(lado, 2)));
+    volumen = (Math.Pow(lado, 2) * altura) / 3;
+    arista = Math.Sqrt(Math.Pow(altura, 2) + (Math.Pow(lado, 2) / 2));
+
+    Console.WriteLine($"el valor volumen de la piramdie cuadrada es: {volumen}");
+    Console.WriteLine($"el valor area de la piramide cuadrada es: {area}");
+    Console.WriteLine($"el valor arista de la piramide cuadrada es: {arista}");
+} while(true);
+
+Console.WriteLine($"se ingresaron {contador} piramides");
